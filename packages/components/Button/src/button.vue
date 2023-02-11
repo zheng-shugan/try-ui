260d9bcdf286b17b5c2e7a38538dda08fe192b1c
syrun <113893385+syrun@users.noreply.github.com>
2023-02-11 23:24:06 +0800

    feat: 更新了 button 组件，完善了相关功能 (#4)

---
+++ packages/components/Button/src/button.vue
@@ -0,0 +1,32 @@
+<script lang="ts" setup>
+import { withDefaults } from 'vue'
+const props = withDefaults(defineProps<{
+  type?: 'primary' | 'success' | 'danger' | 'info' | 'warning' | 'default'
+  round?: boolean
+  circle?: boolean
+  loading?: boolean
+  disabled?: boolean
+  size?: 'medium' | 'small' | 'mini'
+}>(), {
+  type: 'default',
+  round: false,
+  circle: false,
+  loading: false,
+  disabled: false,
+})
+const buttonClass = [
+  't-button',
+  `t-button--${props.type}`,
+  props.size ? `t-button--${props.size}` : '',
+  props.circle ? 'is-circle' : '',
+  props.disabled ? 'is-disabled' : '',
+  props.loading ? 'is-loading' : '',
+  props.round ? 'is-round' : '',
+]
+</script>
+
+<template>
+  <button :class="buttonClass" :disabled="disabled" type="button">
+    <span v-if=" $slots.default "><slot /></span>
+  </button>
+</template>=/
---
+++ packages/components/Button/src/buttontype.d.ts
@@ -0,0 +1,2 @@
+// 用户在使用相关属性时提供类型声明
+/// <reference types="vite/client" />
