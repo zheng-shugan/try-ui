55380fa7e10b3933d969810690aa51df09fcaa3f
Crina0912 <113112331+Crina0912@users.noreply.github.com>
2023-02-21 00:29:16 +0800

    feat(*):新增textarea组件；合并main分支 (#12)
    * feat(*):新增textarea组件
    
    * 测试
    
    ---------
    
    Co-authored-by: XiaoqiaoHuang <2537784487@qq.com>

---
+++ packages/components/textarea/doc/doc.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+        <h2>Textarea文本域</h2>
+        <tTexarea />
+        <preview compName="textarea" demoName="demo1"/>
+    </div>
+</template>
+<script setup>
+import tTexarea from './demo1.vue'
+import preview from '../../../../src/components/preview.vue'
+
+</script>
+<style lang="scss" scoped></style>
