21bfc11911189f967c16a0ffcd54e5420964cd51
Ra-Liz <2879055132@qq.com>
2023-02-21 10:01:15 +0800

    feat:add_input_doc

--- docs/.vitepress/cache/deps/@theme_index.js
+++ docs/.vitepress/cache/deps/@theme_index.js
@@ -1,24 +1,24 @@
 // ../node_modules/.pnpm/registry.npmmirror.com+vitepress@1.0.0-alpha.46/node_modules/vitepress/dist/client/theme-default/index.js
-import "/Users/zhengshugan/FrontEnd/Project/try-ui/node_modules/.pnpm/registry.npmmirror.com+vitepress@1.0.0-alpha.46/node_modules/vitepress/dist/client/theme-default/styles/fonts.css";
-import "/Users/zhengshugan/FrontEnd/Project/try-ui/node_modules/.pnpm/registry.npmmirror.com+vitepress@1.0.0-alpha.46/node_modules/vitepress/dist/client/theme-default/styles/vars.css";
-import "/Users/zhengshugan/FrontEnd/Project/try-ui/node_modules/.pnpm/registry.npmmirror.com+vitepress@1.0.0-alpha.46/node_modules/vitepress/dist/client/theme-default/styles/base.css";
-import "/Users/zhengshugan/FrontEnd/Project/try-ui/node_modules/.pnpm/registry.npmmirror.com+vitepress@1.0.0-alpha.46/node_modules/vitepress/dist/client/theme-default/styles/utils.css";
-import "/Users/zhengshugan/FrontEnd/Project/try-ui/node_modules/.pnpm/registry.npmmirror.com+vitepress@1.0.0-alpha.46/node_modules/vitepress/dist/client/theme-default/styles/components/custom-block.css";
-import "/Users/zhengshugan/FrontEnd/Project/try-ui/node_modules/.pnpm/registry.npmmirror.com+vitepress@1.0.0-alpha.46/node_modules/vitepress/dist/client/theme-default/styles/components/vp-code.css";
-import "/Users/zhengshugan/FrontEnd/Project/try-ui/node_modules/.pnpm/registry.npmmirror.com+vitepress@1.0.0-alpha.46/node_modules/vitepress/dist/client/theme-default/styles/components/vp-code-group.css";
-import "/Users/zhengshugan/FrontEnd/Project/try-ui/node_modules/.pnpm/registry.npmmirror.com+vitepress@1.0.0-alpha.46/node_modules/vitepress/dist/client/theme-default/styles/components/vp-doc.css";
-import "/Users/zhengshugan/FrontEnd/Project/try-ui/node_modules/.pnpm/registry.npmmirror.com+vitepress@1.0.0-alpha.46/node_modules/vitepress/dist/client/theme-default/styles/components/vp-sponsor.css";
-import VPBadge from "/Users/zhengshugan/FrontEnd/Project/try-ui/node_modules/.pnpm/registry.npmmirror.com+vitepress@1.0.0-alpha.46/node_modules/vitepress/dist/client/theme-default/components/VPBadge.vue";
-import Layout from "/Users/zhengshugan/FrontEnd/Project/try-ui/node_modules/.pnpm/registry.npmmirror.com+vitepress@1.0.0-alpha.46/node_modules/vitepress/dist/client/theme-default/Layout.vue";
-import NotFound from "/Users/zhengshugan/FrontEnd/Project/try-ui/node_modules/.pnpm/registry.npmmirror.com+vitepress@1.0.0-alpha.46/node_modules/vitepress/dist/client/theme-default/NotFound.vue";
-import { default as default2 } from "/Users/zhengshugan/FrontEnd/Project/try-ui/node_modules/.pnpm/registry.npmmirror.com+vitepress@1.0.0-alpha.46/node_modules/vitepress/dist/client/theme-default/components/VPHomeHero.vue";
-import { default as default3 } from "/Users/zhengshugan/FrontEnd/Project/try-ui/node_modules/.pnpm/registry.npmmirror.com+vitepress@1.0.0-alpha.46/node_modules/vitepress/dist/client/theme-default/components/VPHomeFeatures.vue";
-import { default as default4 } from "/Users/zhengshugan/FrontEnd/Project/try-ui/node_modules/.pnpm/registry.npmmirror.com+vitepress@1.0.0-alpha.46/node_modules/vitepress/dist/client/theme-default/components/VPHomeSponsors.vue";
-import { default as default5 } from "/Users/zhengshugan/FrontEnd/Project/try-ui/node_modules/.pnpm/registry.npmmirror.com+vitepress@1.0.0-alpha.46/node_modules/vitepress/dist/client/theme-default/components/VPDocAsideSponsors.vue";
-import { default as default6 } from "/Users/zhengshugan/FrontEnd/Project/try-ui/node_modules/.pnpm/registry.npmmirror.com+vitepress@1.0.0-alpha.46/node_modules/vitepress/dist/client/theme-default/components/VPTeamPage.vue";
-import { default as default7 } from "/Users/zhengshugan/FrontEnd/Project/try-ui/node_modules/.pnpm/registry.npmmirror.com+vitepress@1.0.0-alpha.46/node_modules/vitepress/dist/client/theme-default/components/VPTeamPageTitle.vue";
-import { default as default8 } from "/Users/zhengshugan/FrontEnd/Project/try-ui/node_modules/.pnpm/registry.npmmirror.com+vitepress@1.0.0-alpha.46/node_modules/vitepress/dist/client/theme-default/components/VPTeamPageSection.vue";
-import { default as default9 } from "/Users/zhengshugan/FrontEnd/Project/try-ui/node_modules/.pnpm/registry.npmmirror.com+vitepress@1.0.0-alpha.46/node_modules/vitepress/dist/client/theme-default/components/VPTeamMembers.vue";
+import "E:/JUMP/try-ui/node_modules/.pnpm/registry.npmmirror.com+vitepress@1.0.0-alpha.46/node_modules/vitepress/dist/client/theme-default/styles/fonts.css";
+import "E:/JUMP/try-ui/node_modules/.pnpm/registry.npmmirror.com+vitepress@1.0.0-alpha.46/node_modules/vitepress/dist/client/theme-default/styles/vars.css";
+import "E:/JUMP/try-ui/node_modules/.pnpm/registry.npmmirror.com+vitepress@1.0.0-alpha.46/node_modules/vitepress/dist/client/theme-default/styles/base.css";
+import "E:/JUMP/try-ui/node_modules/.pnpm/registry.npmmirror.com+vitepress@1.0.0-alpha.46/node_modules/vitepress/dist/client/theme-default/styles/utils.css";
+import "E:/JUMP/try-ui/node_modules/.pnpm/registry.npmmirror.com+vitepress@1.0.0-alpha.46/node_modules/vitepress/dist/client/theme-default/styles/components/custom-block.css";
+import "E:/JUMP/try-ui/node_modules/.pnpm/registry.npmmirror.com+vitepress@1.0.0-alpha.46/node_modules/vitepress/dist/client/theme-default/styles/components/vp-code.css";
+import "E:/JUMP/try-ui/node_modules/.pnpm/registry.npmmirror.com+vitepress@1.0.0-alpha.46/node_modules/vitepress/dist/client/theme-default/styles/components/vp-code-group.css";
+import "E:/JUMP/try-ui/node_modules/.pnpm/registry.npmmirror.com+vitepress@1.0.0-alpha.46/node_modules/vitepress/dist/client/theme-default/styles/components/vp-doc.css";
+import "E:/JUMP/try-ui/node_modules/.pnpm/registry.npmmirror.com+vitepress@1.0.0-alpha.46/node_modules/vitepress/dist/client/theme-default/styles/components/vp-sponsor.css";
+import VPBadge from "E:/JUMP/try-ui/node_modules/.pnpm/registry.npmmirror.com+vitepress@1.0.0-alpha.46/node_modules/vitepress/dist/client/theme-default/components/VPBadge.vue";
+import Layout from "E:/JUMP/try-ui/node_modules/.pnpm/registry.npmmirror.com+vitepress@1.0.0-alpha.46/node_modules/vitepress/dist/client/theme-default/Layout.vue";
+import NotFound from "E:/JUMP/try-ui/node_modules/.pnpm/registry.npmmirror.com+vitepress@1.0.0-alpha.46/node_modules/vitepress/dist/client/theme-default/NotFound.vue";
+import { default as default2 } from "E:/JUMP/try-ui/node_modules/.pnpm/registry.npmmirror.com+vitepress@1.0.0-alpha.46/node_modules/vitepress/dist/client/theme-default/components/VPHomeHero.vue";
+import { default as default3 } from "E:/JUMP/try-ui/node_modules/.pnpm/registry.npmmirror.com+vitepress@1.0.0-alpha.46/node_modules/vitepress/dist/client/theme-default/components/VPHomeFeatures.vue";
+import { default as default4 } from "E:/JUMP/try-ui/node_modules/.pnpm/registry.npmmirror.com+vitepress@1.0.0-alpha.46/node_modules/vitepress/dist/client/theme-default/components/VPHomeSponsors.vue";
+import { default as default5 } from "E:/JUMP/try-ui/node_modules/.pnpm/registry.npmmirror.com+vitepress@1.0.0-alpha.46/node_modules/vitepress/dist/client/theme-default/components/VPDocAsideSponsors.vue";
+import { default as default6 } from "E:/JUMP/try-ui/node_modules/.pnpm/registry.npmmirror.com+vitepress@1.0.0-alpha.46/node_modules/vitepress/dist/client/theme-default/components/VPTeamPage.vue";
+import { default as default7 } from "E:/JUMP/try-ui/node_modules/.pnpm/registry.npmmirror.com+vitepress@1.0.0-alpha.46/node_modules/vitepress/dist/client/theme-default/components/VPTeamPageTitle.vue";
+import { default as default8 } from "E:/JUMP/try-ui/node_modules/.pnpm/registry.npmmirror.com+vitepress@1.0.0-alpha.46/node_modules/vitepress/dist/client/theme-default/components/VPTeamPageSection.vue";
+import { default as default9 } from "E:/JUMP/try-ui/node_modules/.pnpm/registry.npmmirror.com+vitepress@1.0.0-alpha.46/node_modules/vitepress/dist/client/theme-default/components/VPTeamMembers.vue";
 var theme = {
   Layout,
   NotFound,
--- docs/index.md
+++ docs/index.md
@@ -1,1 +1,37 @@
-# Hello Try UI
+---
+layout: home
+
+hero:
+  name: Try UI
+  text: A component library project under development.
+  tagline: Presented by 尽力局…
+  image:
+    src: "./logo.jpg"   
+    alt: TryUI-Logo
+  actions:
+    - theme: brand
+      text: Get Started
+      link: /introduce
+    - theme: alt
+      text: View on GitHub
+      link: https://github.com/zheng-shugan/try-ui
+features:
+  - icon: 🛠️
+    title: 框架工具
+    details: Vue3 TS Vite …
+  - icon: 💌
+    title: Contributeor
+    details: 大家的昵称
+  - icon:
+      dark: /dark-feature-icon.svg
+      light: /light-feature-icon.svg
+    title: 其他
+    details: 文字   
+---
+
+<style>
+  :root {
+  --vp-home-hero-name-color: transparent;
+  --vp-home-hero-name-background: -webkit-linear-gradient(120deg, #bd34fe, #41d1ff);
+}
+</style>
