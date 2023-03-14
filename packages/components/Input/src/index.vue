aea8a57d3e764ec3f21e75e4961b6297eee644a1
Ra-Liz <2879055132@qq.com>
2023-03-14 16:31:45 +0800

    feat(*):Input组件新增复合型输入框类型、带有Icon组件的输入框类型

--- packages/components/Input/src/index.vue
+++ packages/components/Input/src/index.vue
@@ -1,105 +1,128 @@
 <script lang="ts" setup>
-import { computed, ref, useAttrs } from 'vue'
+  import { computed, ref, useAttrs, useSlots } from 'vue'
 
-// withDefaults可以为props添加默认值等
-const inputProps = withDefaults(defineProps<InputProps>(), {
-  modelValue: '',
-})
-const inputEmits = defineEmits<InputEmits>()
-type TargetElement = HTMLInputElement | HTMLTextAreaElement
+  const inputProps = withDefaults(defineProps<InputProps>(), {
+    modelValue: '',
+  })
+  const inputEmits = defineEmits<InputEmits>()
+  // 实现props穿透，继承原生input属性
+  const attrs = useAttrs()
+  // 复合输入框
+  const slots = useSlots()
 
-// 实现props穿透，继承原生input属性
-const attrs = useAttrs()
+  type TargetElement = HTMLInputElement | HTMLTextAreaElement
 
-// 组件接收的值类型
-interface InputProps {
-  modelValue?: string | number
-  disabled?: boolean
-  size?: 'mini' | 'small' | 'medium'
-  clearable?: boolean
-  showPassword?: boolean
-}
+  // 组件接收的值类型
+  interface InputProps {
+    modelValue?: string | number
+    disabled?: boolean
+    size?: 'mini' | 'small' | 'medium'
+    clearable?: boolean
+    showPassword?: boolean
+    prefixIcon?: string
+    suffixIcon?: string
+  }
 
-// 组件发送事件类型
-type InputEmits = (e: 'update:modelValue', value: string) => void
+  // 组件发送事件类型
+  type InputEmits = (e: 'update:modelValue', value: string) => void
 
-// 清除inputValue
-const isEnter = ref(true)
-const isClearAbled = ref(false)
-const clearValue = () => {
-  inputEmits('update:modelValue', '')
-}
+  // 清除inputValue
+  const isEnter = ref(true)
+  const isClearAbled = ref(false)
+  const clearValue = () => {
+    inputEmits('update:modelValue', '')
+  }
 
-// 密码框显示
-const ipt = ref()
-Promise.resolve().then(() => {
-  if (inputProps.showPassword)
+  // 密码框显示
+  const ipt = ref()
+  Promise.resolve().then(() => {
+    if (inputProps.showPassword)
+      ipt.value.type = 'password'
+  })
+  const isShowEye = computed(() => {
+    return inputProps.showPassword && inputProps.modelValue && !inputProps.clearable
+  })
+  const changeType = () => {
+    if (ipt.value.type === 'password') {
+      ipt.value.type = attrs.type || 'text'
+      return
+    }
     ipt.value.type = 'password'
-})
-const isShowEye = computed(() => {
-  return inputProps.showPassword && inputProps.modelValue && !inputProps.clearable
-})
-const changeType = () => {
-  if (ipt.value.type === 'password') {
-    ipt.value.type = attrs.type || 'text'
-    return
   }
-  ipt.value.type = 'password'
-}
 
-// 输入值更新
-const changeInputVal = (event: Event) => {
-  // 清除clearable
-  (event.target as HTMLInputElement).value ? (isClearAbled.value = true) : (isClearAbled.value = false)
-  // 输入事件：数据双向绑定
-  inputEmits('update:modelValue', (event.target as TargetElement).value)
-}
-
-// 禁用样式
-const styleClass = computed(() => {
-  return {
-    'is-disabled': inputProps.disabled,
-    [`t-input--${inputProps.size}`]: inputProps.size,
+  // 输入值更新
+  const changeInputVal = (event: Event) => {
+    // 清除clearable
+    (event.target as HTMLInputElement).value ? (isClearAbled.value = true) : (isClearAbled.value = false)
+    // 输入事件：数据双向绑定
+    inputEmits('update:modelValue', (event.target as TargetElement).value)
   }
-})
+
+  // 增加Icon组件的输入框
+  // -前置
+  const isShowPrefixIcon = computed(() => {
+    return inputProps.prefixIcon
+  })  
+  // -后置
+  const isShowSuffixIcon = computed(() => {
+    return (inputProps.suffixIcon && !inputProps.clearable && !inputProps.showPassword)
+  })
+
+
+  // 传递样式
+  const styleClass = computed(() => {
+    return {
+      // 禁用
+      'is-disabled': inputProps.disabled,
+      // 大小
+      [`t-input--${inputProps.size}`]: inputProps.size,
+      // 复合输入框-前
+      ["t-input-group t-input-prepend"]: slots.prepend,
+      // 复合输入框-后
+      ["t-input-group t-input-append"]: slots.append
+    }
+  })
 </script>
 
 <template>
-  <div
-    class="t-input"
-    :class="styleClass"
-    @mouseenter="isEnter = true"
-    @mouseleave="isEnter = false"
-  >
+  <div class="t-input" :class="styleClass" @mouseenter="isEnter = true" @mouseleave="isEnter = false">
+    <!-- 前置复合控件 -->
+    <div class="t-input__prepend" v-if="slots.prepend">
+      <slot name="prepend"></slot>
+    </div>
+
+    <!-- 输入框本框 -->
     <input
       ref="ipt"
       class="t-input__inner"
-      placeholder="请输入文字"
       :value="inputProps.modelValue"
       :disabled="inputProps.disabled"
       v-bind="attrs"
       @input="changeInputVal"
     >
-    <!-- 一键删除 -->
-    <div
-      v-if="inputProps.clearable && isClearAbled"
-      class="t-input__suffix"
-      @click="clearValue"
-    >
+
+    <!-- 删除控件，后面图标会改成Icon -->
+    <div v-if="inputProps.clearable && isClearAbled" class="t-input__suffix" @click="clearValue">
       <img src="../style/error.png">
     </div>
-    <!-- 是否可见 -->
-    <div
-      v-show="isShowEye"
-      class="t-input__suffix"
-      @click="changeType"
-    >
-      <!-- 待完善 -->
+    <!-- 展示控件，后面图标会改成Icon -->
+    <div v-show="isShowEye" class="t-input__suffix" @click="changeType">
       <img src="../style/Show.png" alt="密码可见">
     </div>
-    <!-- <p>value：{{ inputProps.modelValue }}</p> -->
+
+    <!-- 图标控件 -->
+    <div v-if="isShowPrefixIcon" class="t-input__prefix no-cursor">
+      <t-icon :type="inputProps.prefixIcon" />
+    </div>
+    <div v-if="isShowSuffixIcon" class="t-input__suffix no-cursor">
+      <t-icon :type="inputProps.suffixIcon" />
+    </div>
+
+    <!-- 后置复合控件 -->
+    <div class="t-input__append" v-if="slots.append">
+      <slot name="append"></slot>
+    </div>
   </div>
 </template>
 
-<style lang="scss" src="../style/style.scss">
-</style>
+<style lang="scss" src="../style/style.scss"></style>
--- src/components/Input.vue
+++ src/components/Input.vue
@@ -4,7 +4,10 @@ export default {
   data() {
     return {
       // 双向绑定数据
-      value: '',
+      value1: '',
+      value2: '',
+      value3: '',
+      value4: '',
       // 单向绑定数据
       bindValue: '啊哈我被单向绑定啦！',
       // 输入框尺寸
@@ -23,22 +26,22 @@ export default {
     <p /><p />
     <h3>基础用法</h3>
     <em>实现输入框输入数据的双向绑定。</em><br>
-    <t-input v-model="value" />
+    <t-input v-model="value1" />
     <p>备注：这里可传入value</p>
     <p /><p />
     <h3>禁用状态</h3>
     <em>实现禁用输入框，无法编辑输入框内文本。</em><br>
-    <t-input v-model="value" :disabled="true" />
+    <t-input v-model="value2" :disabled="true" />
     <p>备注：这里可传入disabled true时输入框被禁用</p>
     <p /><p />
     <h3>非正式-不同尺寸</h3>
     <em>除了默认尺寸，Tinput额外定义了三种输入框尺寸，它们分别是medium,small,mini</em><br>
     <em>size=medium</em><br>
-    <t-input v-model="value" :size="medium" /><br>
+    <t-input :size="medium" /><br>
     <em>size=small</em><br>
-    <t-input v-model="value" :size="small" /><br>
+    <t-input :size="small" /><br>
     <em>size=mini</em><br>
-    <t-input v-model="value" :size="mini" /><br>
+    <t-input :size="mini" /><br>
     <p>备注：这里可传入size</p>
     <p /><p />
     <h3>非正式-v-bind绑定</h3>
@@ -48,13 +51,42 @@ export default {
     <p /><p />
     <h3>可清空</h3>
     <em>点击图标，实现输入框内容清空</em><br>
-    <t-input v-model="value" :clearable="true" />
+    <t-input v-model="value3" :clearable="true" />
     <p>备注：这里可传入clearable,true可清除</p>
     <p /><p />
     <h3>密码框</h3>
     <em>点击图标，对界面显示/隐藏密码</em><br>
-    <t-input v-model="value" :show-password="true" />
+    <t-input v-model="value4" :show-password="true" />
     <p>备注：这里可传入show-password,true启用</p>
+
+    <p /><p />
+    <h3>带图标的输入框</h3>
+    <em>那么接下来就不得不请出Input+Icon的小小联动体了</em><br>
+    <em>让我想想应该如何去实现</em><br>
+    <t-input prefixIcon="calendar" />
+    <br /><br />
+    <t-input suffixIcon="psw" />
+    <p>备注：这里可传入show-password,true启用</p>
+
+    <p /><p />
+    <h3>复合型输入框</h3>
+    <em>支持复合型输入框，你仅需要在t-input标签内使用：template #prepend(前置)/append(后置)</em><br>
+    <em>你可以试一下在template中插入其他组件，比如Icon什么的，当然有以下组件我们还在开发中w</em><br>
+    <div>
+      <t-input placeholder="请输入网址">
+        <template #prepend>
+          https://
+        </template>
+      </t-input>
+    </div>
+    <br />
+    <div>
+      <t-input placeholder="请输入网址">
+        <template #append>
+          .com
+        </template>
+      </t-input>
+    </div>
   </div>
 </template>
 
