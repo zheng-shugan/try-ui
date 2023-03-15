ab8dc580a53e7113be939b0c78de6984fa0ca878
Ra-Liz <86996381+Ra-Liz@users.noreply.github.com>
2023-03-15 19:03:45 +0800

    refactor(docs):更新了文档导航栏、侧边栏等细节 (#34)
    * feat: 新增Dialog组件 refactor(docs): 新增Dialog文档
    
    * feat: 增加Dialog组件 refactor(docs): 增加Dialog文档
    
    * refactor(docs):修改了Input文档
    
    * refactor(docs):更新了文档导航栏、侧边栏布局等细节

--- packages/components/Input/src/index.vue
+++ packages/components/Input/src/index.vue
@@ -1,94 +1,91 @@
 <script lang="ts" setup>
-  import { computed, ref, useAttrs, useSlots } from 'vue'
+import { computed, ref, useAttrs, useSlots } from 'vue'
 
-  const inputProps = withDefaults(defineProps<InputProps>(), {
-    modelValue: '',
-  })
-  const inputEmits = defineEmits<InputEmits>()
-  // 实现props穿透，继承原生input属性
-  const attrs = useAttrs()
-  // 复合输入框
-  const slots = useSlots()
+const inputProps = withDefaults(defineProps<InputProps>(), {
+  modelValue: '',
+})
+const inputEmits = defineEmits<InputEmits>()
+// 实现props穿透，继承原生input属性
+const attrs = useAttrs()
+// 复合输入框
+const slots = useSlots()
 
-  type TargetElement = HTMLInputElement | HTMLTextAreaElement
+type TargetElement = HTMLInputElement | HTMLTextAreaElement
 
-  // 组件接收的值类型
-  interface InputProps {
-    modelValue?: string | number
-    disabled?: boolean
-    size?: 'mini' | 'small' | 'medium'
-    clearable?: boolean
-    showPassword?: boolean
-    prefixIcon?: string
-    suffixIcon?: string
-  }
+// 参数——类型
+interface InputProps {
+  modelValue?: string | number
+  disabled?: boolean
+  size?: 'mini' | 'small' | 'medium'
+  clearable?: boolean
+  showPassword?: boolean
+  prefixIcon?: string
+  suffixIcon?: string
+}
 
-  // 组件发送事件类型
-  type InputEmits = (e: 'update:modelValue', value: string) => void
+// 事件——类型
+type InputEmits = (e: 'update:modelValue', value: string) => void
 
-  // 清除inputValue
-  const isEnter = ref(true)
-  const isClearAbled = ref(false)
-  const clearValue = () => {
-    inputEmits('update:modelValue', '')
-  }
+// 清除功能
+const isEnter = ref(true)
+const isClearAbled = ref(false)
+const clearValue = () => {
+  inputEmits('update:modelValue', '')
+}
 
-  // 密码框显示
-  const ipt = ref()
-  Promise.resolve().then(() => {
-    if (inputProps.showPassword)
-      ipt.value.type = 'password'
-  })
-  const isShowEye = computed(() => {
-    return inputProps.showPassword && inputProps.modelValue && !inputProps.clearable
-  })
-  const changeType = () => {
-    if (ipt.value.type === 'password') {
-      ipt.value.type = attrs.type || 'text'
-      return
-    }
+// 密码框功能
+const ipt = ref()
+Promise.resolve().then(() => {
+  if (inputProps.showPassword)
     ipt.value.type = 'password'
+})
+const isShowEye = computed(() => {
+  return inputProps.showPassword && inputProps.modelValue && !inputProps.clearable
+})
+const changeType = () => {
+  if (ipt.value.type === 'password') {
+    ipt.value.type = attrs.type || 'text'
+    return
   }
+  ipt.value.type = 'password'
+}
 
-  // 输入值更新
-  const changeInputVal = (event: Event) => {
-    // 清除clearable
-    (event.target as HTMLInputElement).value ? (isClearAbled.value = true) : (isClearAbled.value = false)
-    // 输入事件：数据双向绑定
-    inputEmits('update:modelValue', (event.target as TargetElement).value)
+// 数据更新功能
+const changeInputVal = (event: Event) => {
+  // 清除clearable
+  (event.target as HTMLInputElement).value ? (isClearAbled.value = true) : (isClearAbled.value = false)
+  // 输入事件：数据双向绑定
+  inputEmits('update:modelValue', (event.target as TargetElement).value)
+}
+
+// 带Icon组件的输入框功能
+const isShowPrefixIcon = computed(() => {
+  return inputProps.prefixIcon
+})
+const isShowSuffixIcon = computed(() => {
+  return (inputProps.suffixIcon && !inputProps.clearable && !inputProps.showPassword)
+})
+
+// 传递样式
+const styleClass = computed(() => {
+  return {
+    // 禁用
+    'is-disabled': inputProps.disabled,
+    // 大小
+    [`t-input--${inputProps.size}`]: inputProps.size,
+    // 复合输入框-前
+    't-input-group t-input-prepend': slots.prepend,
+    // 复合输入框-后
+    't-input-group t-input-append': slots.append,
   }
-
-  // 增加Icon组件的输入框
-  // -前置
-  const isShowPrefixIcon = computed(() => {
-    return inputProps.prefixIcon
-  })  
-  // -后置
-  const isShowSuffixIcon = computed(() => {
-    return (inputProps.suffixIcon && !inputProps.clearable && !inputProps.showPassword)
-  })
-
-
-  // 传递样式
-  const styleClass = computed(() => {
-    return {
-      // 禁用
-      'is-disabled': inputProps.disabled,
-      // 大小
-      [`t-input--${inputProps.size}`]: inputProps.size,
-      // 复合输入框-前
-      ["t-input-group t-input-prepend"]: slots.prepend,
-      // 复合输入框-后
-      ["t-input-group t-input-append"]: slots.append
-    }
-  })
+})
 </script>
 
 <template>
   <div class="t-input" :class="styleClass" @mouseenter="isEnter = true" @mouseleave="isEnter = false">
     <!-- 前置复合控件 -->
-    <div class="t-input__prepend" v-if="slots.prepend">
-      <slot name="prepend"></slot>
+    <div v-if="slots.prepend" class="t-input__prepend">
+      <slot name="prepend" />
     </div>
 
     <!-- 输入框本框 -->
@@ -119,8 +116,8 @@
     </div>
 
     <!-- 后置复合控件 -->
-    <div class="t-input__append" v-if="slots.append">
-      <slot name="append"></slot>
+    <div v-if="slots.append" class="t-input__append">
+      <slot name="append" />
     </div>
   </div>
 </template>
