88043502854b3eb36da304f409c1b0bd1b5da9ec
Ra-Liz <86996381+Ra-Liz@users.noreply.github.com>
2023-03-29 08:46:54 +0800

    fix(*):修复了样式难看的Icon组件，补充了组件功能 (#39)

--- packages/components/Icon/src/index.vue
+++ packages/components/Icon/src/index.vue
@@ -1,28 +1,86 @@
-<template>
-    <svg class="icon" aria-hidden="true">
-        <use v-bind:xlink:href="`#icon-${props.type}`"></use>
-    </svg>
-</template>
-
 <script lang="ts" setup>
-    const props = defineProps({
-        type: {
-            type: String,
-            required : true
-        },
-    })
+import { computed } from 'vue'
+
+const props = defineProps({
+  type: {
+    type: String,
+    required: true,
+  },
+  color: {
+    type: String,
+    default: '',
+  },
+  dot: {
+    type: Boolean,
+    default: false,
+  },
+  badage: {
+    type: String,
+    default: '',
+  },
+})
+const styleDot = computed(() => {
+  return {
+    'icon-dot': props.dot && !props.badage,
+  }
+})
+const iconColor = computed(() => {
+  return {
+    color: props.color,
+  }
+})
 </script>
+
+<template>
+  <div class="icon">
+    <svg class="icon-img" aria-hidden="true" :style="iconColor">
+      <use :xlink:href="`#icon-${props.type}`" />
+    </svg>
+    <div v-if="dot" class="icon-info" :class="styleDot">
+      {{ badage }}
+    </div>
+  </div>
+</template>
 
 <style scoped>
 i {
-    margin-right: 4px;
-    vertical-align: middle;
+  margin-right: 4px;
+  vertical-align: middle;
 }
 
 .icon {
-    width: 4em; height: 4em;
-    vertical-align: -0.15em;
-    fill: currentColor;
-    overflow: hidden;
- }
+  display: inline-block;
+  position: relative;
+}
+.icon-img {
+  overflow: hidden;
+  fill: currentColor;
+  width: 4em;
+  height: 4em;
+}
+.icon-info {
+  position: absolute;
+  top: 1em;
+  right: 1em;
+  transform: translate(50%, -50%);
+  box-sizing: border-box;
+  min-width: 16px;
+  padding: 03px;
+  color: #fff;
+  font-weight: 500;
+  font-size: 12px;
+  line-height: 1.2;
+  text-align: center;
+  /* border: 1px solid #fff; */
+  border-radius: 16px;
+  background-color: #ee0a24;
+}
+
+.icon-dot {
+  width: 8px;
+  min-width: 0;
+  height: 8px;
+  background-color: #ee0a24;
+  border-radius: 100%;
+}
 </style>
