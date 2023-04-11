68bff75cd8b4c30e9d6163ccf71cf968b886edec
zhengshugan <zhengshugan@foxmail.com>
2023-04-11 10:28:59 +0800

    feat(result): 新增 result 组件

---
+++ packages/components/Result/src/index.vue
@@ -0,0 +1,29 @@
+<script lang='ts' setup>
+import { defineProps } from 'vue'
+
+interface ResultProps {
+  title: string
+  subTitle: string
+}
+
+const props = defineProps<ResultProps>()
+</script>
+
+<template>
+  <div>
+    <div v-if="title || $slots.title">
+      <slot name="title">
+        <p>{{ title }}</p>
+      </slot>
+    </div>
+    <div v-if="subTitle || $slots.subTitle">
+      <slot name="sub-title">
+        <p>{{ subTitle }}</p>
+      </slot>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
